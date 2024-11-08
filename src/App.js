c3fd54df9fb6e3764583f4c28ad40b07d22a5586
Yuchan Kim <yuchankim0906@gmail.com>
2024-11-08 11:11:50 +0900

    feat(ui - 챌린지 공지사항, 통계, 성적표) 화면들의 ui를 생성하였습니다

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import YCChallengeStatistics from './yc_pages/YC_challenge_statistics';
+import YcChallengeBoard from './yc_pages/YC_challenge_board';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<YCChallengeStatistics/>} />
+        </Routes>
+    </BrowserRouter>
+
+      );
 }
 
 export default App;
