8f3e4566519bb548b2242ce1bd67bb2370bfedac
함민규 <antrophus@yahoo.com>
2024-11-19 10:48:42 +0900

    feat(mypage):11/19 보관함 페이지 구성(사용가능/사용완료) 변경, api 연결 확인

--- src/ham_data/ham_giftData.jsx
+++ src/ham_data/ham_giftData.jsx
@@ -10,12 +10,12 @@ import GiftImage7 from '../ham_asset/images/gift7.jpg';
 // ... 다른 기프티콘 이미지 import (추가 필요 시 여기에 추가)
 
 export const giftItems = [
-    { id: 1, name: "눼눼칙힌", image: GiftImage1 },
-    { id: 2, name: "BBQ", image: GiftImage2 },
-    { id: 3, name: "피자혓", image: GiftImage3 },
-    { id: 4, name: "나눔로또", image: GiftImage4 },
-    { id: 5, name: "칙힌깊흐트", image: GiftImage5 },
-    { id: 6, name: "배라기프티콘", image: GiftImage6 },
-    { id: 7, name: "치킨기프트", image: GiftImage7 },
+    { id: 1, name: "눼눼칙힌", image: GiftImage1, description: "눼눼칙힌에서 사용할 수 있는 기프티콘입니다.", isUsed: false, },
+    { id: 2, name: "BBQ", image: GiftImage2, description: "BBQ에서 사용할 수 있는 기프티콘입니다.", isUsed: true },
+    { id: 3, name: "피자혓", image: GiftImage3, description: "피자혓에서 사용할 수 있는 기프티콘입니다.", isUsed: false },
+    { id: 4, name: "나눔로또", image: GiftImage4, description: "나눔로또 기프티콘입니다.", isUsed: true },
+    { id: 5, name: "칙힌깊흐트", image: GiftImage5, description: "칙힌을 먹을 수 있는 기프티콘입니다.", isUsed: true },
+    { id: 6, name: "배라기프티콘", image: GiftImage6, description: "배숙희라임스에서 사용할 수 있는 기프티콘입니다.", isUsed: false },
+    { id: 7, name: "치킨기프트", image: GiftImage7, description: "치킨 기프티콘입니다.", isUsed: false },
     // ... 추가 기프티콘 객체 (필요 시 여기에 추가)
 ];
