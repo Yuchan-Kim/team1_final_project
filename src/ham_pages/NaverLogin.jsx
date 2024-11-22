7bf3b9158f2f48073f61ce6a2101f9cc0678bbd5
함민규 <antrophus@yahoo.com>
2024-11-22 18:40:17 +0900

    chore(.env) : 11/22 깃 허브에 환경변수 첨부

--- src/ham_pages/NaverLogin.jsx
+++ src/ham_pages/NaverLogin.jsx
@@ -1,6 +1,5 @@
 // NaverLogin.js
 import React, { useEffect, useRef } from 'react';
-// import '../ham_asset/css/ham_naverlogin.css';  // CSS 파일 추가
 
 const NaverLogin = () => {
     const buttonRef = useRef(null);
@@ -9,18 +8,18 @@ const NaverLogin = () => {
         const script = document.createElement("script");
         script.src = "https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js";
         script.charset = "utf-8";
-
+        
         script.onload = () => {
             if (window.naver_id_login && buttonRef.current) {
-                const naver_id_login = new window.naver_id_login(   // window. 추가
+                const naver_id_login = new window.naver_id_login(
                     process.env.REACT_APP_NAVER_CLIENT_ID,
-                    "http://localhost:3000/naver/callback"
+                    `${process.env.REACT_APP_NAVER_CALLBACK_URL}`  // 콜백 URL
                 );
                 naver_id_login.setButton("white", 3, 40);
-                naver_id_login.setDomain("http://localhost:3000");
+                naver_id_login.setDomain(process.env.REACT_APP_NAVER_BASE_URL);
                 naver_id_login.init_naver_id_login();
             }
-            // 버튼 스타일 초기화
+            
             const loginButton = document.getElementById("naver_id_login").firstChild;
             if (loginButton) {
                 loginButton.style.display = 'block';
@@ -39,4 +38,3 @@ const NaverLogin = () => {
 };
 
 export default NaverLogin;
-
